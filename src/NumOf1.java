e820a9d9bb6312ab57e434a870a332d2ac914028
ZXoho <348105852@qq.com>
2020-03-08 15:29:49 +0800

    二进制中1的个数

---
+++ src/NumOf1.java
@@ -0,0 +1,23 @@
+/**
+ * Created by zxoho on 2020/3/8 0008
+ * Description:输入一个整数，输出该数二进制表示中1的个数。
+ * 其中负数用补码表示
+ */
+public class NumOf1 {
+
+    public static void main(String[] args) {
+        NumOf1 test = new NumOf1();
+        System.out.println(test.function(15));
+    }
+
+    public int function(int n){
+        int count = 0;
+        if(n == 0)
+            return count;
+        while (n!=0){
+            count++;
+            n = n & (n-1);
+        }
+        return count;
+    }
+}
