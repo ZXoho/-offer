b0640544effaf1850367b3fbd4a845f89b9aec4c
ZXoho <348105852@qq.com>
2020-03-09 15:29:25 +0800

    死锁例子

---
+++ src/DeadLock.java
@@ -0,0 +1,49 @@
+/**
+ * Created by zxoho on 2020/3/9 0009
+ * Description:产生死锁的例子
+ */
+public class DeadLock {
+
+    public static String str1 = "str1";
+    public static String str2 = "str2";
+
+    public static void main(String[] args) {
+        Thread a = new Thread(() -> {
+            try {
+                while (true) {
+                    synchronized (DeadLock.str1) {
+                        System.out.println(Thread.currentThread().getName() + "锁住了" + str1);
+                        Thread.sleep(1000);
+                        synchronized (DeadLock.str2) {
+                            System.out.println(Thread.currentThread().getName() + "锁住了" + str2);
+                            Thread.sleep(1000);
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread b = new Thread(() -> {
+            try {
+                while (true) {
+                    synchronized (DeadLock.str2) {
+                        System.out.println(Thread.currentThread().getName() + "锁住了" + str2);
+                        Thread.sleep(1000);
+                        synchronized (DeadLock.str1) {
+                            System.out.println(Thread.currentThread().getName() + "锁住了" + str1);
+                            Thread.sleep(1000);
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        a.start();
+        b.start();
+    }
+}
---
+++ src/FindKthToTail.java
@@ -0,0 +1,23 @@
+import DataStructure.ListNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zxoho on 2020/3/8 0008
+ * Description:
+ */
+public class FindKthToTail {
+
+    public ListNode function(ListNode head, int k) {
+        int len = 0;
+        Map<Integer, ListNode> nodes = new HashMap<>();
+        while(head!=null){
+            nodes.put(len, head);
+            head = head.next;
+            len++;
+        }
+        return nodes.get(len-k);
+
+    }
+}
