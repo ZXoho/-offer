e710ccb7b2e314ebc77720e867d368d9d35cf958
ZXoho <348105852@qq.com>
2020-03-10 17:13:08 +0800

    层次遍历二叉树

---
+++ src/DataStructure/TreeNode.java
@@ -0,0 +1,17 @@
+package DataStructure;
+
+/**
+ * Created by zxoho on 2020/3/4 0004
+ * Description: 定义二叉树
+ */
+public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+
+     public TreeNode(){
+     }
+     public TreeNode(int val){
+         this.val = val;
+     }
+}
---
+++ src/PrintTreeFromTopToBottom.java
@@ -0,0 +1,31 @@
+import DataStructure.TreeNode;
+
+import java.util.*;
+
+/**
+ * Created by zxoho on 2020/3/10 0010
+ * Description:从上往下打印出二叉树的每个节点，同层节点从左至右打印
+ * 层次遍历
+ */
+public class PrintTreeFromTopToBottom {
+
+    public ArrayList<Integer> function1(TreeNode root){
+        ArrayList<Integer> result = new ArrayList<>();
+        if(root==null)
+            return result;
+        Queue<TreeNode> nodes = new LinkedList<TreeNode>();
+        nodes.add(root);
+        while(!nodes.isEmpty()){
+            TreeNode temp = nodes.remove();
+            result.add(temp.val);
+            if(temp.left != null){
+                nodes.add(temp.left);
+            }
+            if(temp.right !=null){
+                nodes.add(temp.right);
+            }
+        }
+        return result;
+    }
+
+}
